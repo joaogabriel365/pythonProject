accbbb170aa5b2eb03b16be4c3c895195b658d37
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-20 20:55:21 -0300

    Aula de condições completa.

---
+++ goao project.py
@@ -0,0 +1,44 @@
+#velocidade = int(input("Entre com sua velocidade: "))
+#if velocidade > 80:
+ #   multa = (velocidade - 80) * 5
+  #  print(f" Voce foi multado em {multa} reais")
+#else:
+   # print ("Voce nao foi multado")
+
+   # EX 2
+
+# a = float(input("valor 1:"))
+# b = float(input("valor 2:"))
+# c = float(input("valor 3:"))
+# if a > b and a > c:
+#    print (f"o maior numero é {a}")
+# elif b > c and b > a:
+#    print (f"o maior numero é {b}")
+# elif c > b and c > a:
+#    print (f"o maior numero é {c}")
+# if a < b and a < c:
+#    print (f"o menor numero é {a}")
+# elif b < c and b < a:
+#    print (f"o menor numero é {b}")
+# elif c < b and c < a:
+#    print (f"o menor numero é {c}")
+
+ #EX 3
+
+valor = int(input("Entre com seu salario:"))
+aumento = valor * 0.10
+aumentoB= valor * 0.15
+if valor > 1250:
+ print (f" o aumento calculado foi {valor + aumento}")
+elif valor < 1250:
+ print (f" o aumento calculado foi {valor + aumentoB}")
+
+
+
+
+
+
+
+
+
+
